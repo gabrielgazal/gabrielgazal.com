b4f9450dc4e91ece7b249204f257413a87f3ae0c
Gabriel Gazal <gabrielrgrocha14@gmail.com>
2025-09-10 18:24:33 -0300

    fixed navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,39 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import styles from "../styles/NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
+  const [hovered, setHovered] = useState('');
+
   const navItems = [
     { name: "About", href: "/about" },
-    // { name: "Articles", href: "/articles" },
     { name: "Projects", href: "/projects" },
     { name: "Uses", href: "/uses" }
   ];
 
   return (
     <nav className={styles.navbar}>
-      {navItems.map((item) => (
-        <Link
-          key={item.href}
-          href={item.href}
-          className={`${styles.navItem} ${router.pathname === item.href ? styles.active : ""
-            }`}
-        >
-          {item.name}
-        </Link>
-      ))}
+      {navItems.map((item) => {
+        const isActive = router.pathname === item.href;
+        const isHovered = hovered === item.name;
+
+        return (
+          <Link
+            key={item.href}
+            href={item.href}
+            className={`${styles.navItem} ${isActive ? styles.active : ""}`}
+            onMouseEnter={() => setHovered(item.name)}
+            onMouseLeave={() => setHovered('')}
+          >
+            {isHovered && (
+              <span className={styles.navHovered}></span>
+            )}
+            {item.name}
+          </Link>
+        );
+      })}
     </nav>
   );
 }
