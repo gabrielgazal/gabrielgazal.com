32a65e2a3a659824e96c17f1e627de464809939e
Gabriel Gazal <gabrielrgrocha14@gmail.com>
2025-09-12 15:26:05 -0300

    added task to send email

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,20 +1,23 @@
-const resend = new Resend(process.env.RESEND_API_KEY)
-import EmailTemplate from '../../components/EmailTemplate.js'
+import { Resend } from 'resend';
+import EmailTemplate from '../../components/EmailTemplate.js';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function sendEmail(req, res) {
-  // try {
-  //   const data = req.body
+  try {
+    const data = req.body;
 
-  //   await resend.sendEmail({
-  //     from: 'zenorocha.com <website@zenorocha.com>',
-  //     to: process.env.RESEND_DESTINATION_EMAIL,
-  //     replyTo: data.email,
-  //     subject: `${data.name} - via zenorocha.com`,
-  //     react: <EmailTemplate {...data} />,
-  //   })
+    const response = await resend.emails.send({
+      from: 'website@gabrielgazal.com',
+      to: process.env.RESEND_DESTINATION_EMAIL,
+      reply_to: data.email,
+      subject: `${data.name} - via gabrielgazal.com`,
+      react: <EmailTemplate {...data} />,
+    });
 
-  //   res.status(200).json({ message: 'Email sent' })
-  // } catch (e) {
-  //   res.status(500).json({ message: e.message })
-  // }
+    res.status(200).json({ message: 'Email sent', response });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message });
+  }
 }
